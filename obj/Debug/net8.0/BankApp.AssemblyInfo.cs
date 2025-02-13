e9e454e6b402ceccbfe222ddbbe5af0cbd4980d1
nicorasck <nicholas.rasck@gmail.com>
2025-02-14 00:44:37 +0100

    Created Admin page

--- obj/Debug/net8.0/BankApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e2691504b6c74666a4ed5a9003230fb2ff126c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42454e7c141cf33d129aeaef2218fe46c8837328")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
