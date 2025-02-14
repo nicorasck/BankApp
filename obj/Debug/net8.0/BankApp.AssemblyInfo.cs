c5781a2e50854f2775d40e9bbd5dfaf796498949
nicorasck <nicholas.rasck@gmail.com>
2025-02-14 12:02:27 +0100

    Added buttons in Admin-page

--- obj/Debug/net8.0/BankApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42454e7c141cf33d129aeaef2218fe46c8837328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e454e6b402ceccbfe222ddbbe5af0cbd4980d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
