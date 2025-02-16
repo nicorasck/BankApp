8f60efd27f03419b2462b4c3f6f64e949c6bb5d4
nicorasck <nicholas.rasck@gmail.com>
2025-02-17 00:23:00 +0100

    Adding Bank Account etc.

--- obj/Debug/net8.0/BankApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5781a2e50854f2775d40e9bbd5dfaf796498949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa92ae8ae7cd7f53bf6bdb8151a6eff846916239")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
